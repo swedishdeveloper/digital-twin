5b58173bf5575a6e721ec822ee49ddb1f03f23a3
Christian Landgren <christian@landgren.nu>
2024-09-07 21:48:43 +0200

    refactor: simplify car emission logic and improve filtering in the cars route

--- packages/simulator/web/routes/cars.ts
+++ packages/simulator/web/routes/cars.ts
@@ -7,28 +7,72 @@ import {
   groupBy,
   windowTime,
   Observable,
+  pipe,
 } from 'rxjs'
 import { Socket } from 'socket.io'
 import { Experiment } from '../../../../types/Experiment'
 import Vehicle from '../../models/vehicles/Vehicle'
 
-const register = (experiment: Experiment, socket: Socket): any[] => {
+function EmitOneVehiclePer(ms) {
+  return pipe(
+    windowTime<Vehicle>(ms),
+    mergeMap((win: Observable<Vehicle>) =>
+      win.pipe(
+        groupBy((car) => car.id),
+        mergeMap((cars) => cars.pipe(last()))
+      )
+    )
+  )
+}
+
+const filterCars =
+  ({
+    emitBusUpdates,
+    emitTaxiUpdates,
+    emitCars,
+  }: {
+    emitBusUpdates: boolean
+    emitTaxiUpdates: boolean
+    emitCars: boolean
+  }) =>
+  (car: Vehicle) => {
+    if (!car) return false
+    if (car.vehicleType === 'bus' && !emitBusUpdates) return false
+    if (car.vehicleType === 'taxi' && !emitTaxiUpdates) return false
+    if (car.vehicleType === 'car' && !emitCars) return false
+    return true
+  }
+
+const toJsonWithExperimentId = (experiment) => (vehicle: Vehicle) => ({
+  experimentId: experiment.parameters.id,
+  ...vehicle.toJson(),
+})
+
+function emitCars(socket: Socket) {
+  return (cars: any[]) => {
+    if (!cars.length) return
+    socket.volatile.emit('cars', cars)
+  }
+}
+
+export const register = (experiment: Experiment, socket: Socket): any[] => {
   return [
     experiment.cars
       .pipe(map((vehicle: Vehicle) => vehicle.toJson()))
       .subscribe((car) => {
         socket.emit('cars', [car])
       }),
-    experiment.carUpdates.pipe(
-      windowTime<Vehicle>(100),
-      mergeMap(processWindow),
-      filter(filterCars),
-      map(toJsonWithExperimentId),
-      bufferTime(100, null, 100)
-    ).subscribe(emitCars(socket)),
+    experiment.carUpdates
+      .pipe(
+        EmitOneVehiclePer(100),
+        filter(filterCars(socket.data)),
+        map(toJsonWithExperimentId(experiment)),
+        bufferTime(100, null, 100)
+      )
+      .subscribe(emitCars(socket)),
     experiment.buses
       .pipe(
-        map(cleanCars),
+        map((vehicle: Vehicle) => vehicle.toJson()),
         map((vehicle: Vehicle) => ({
           experimentId: experiment.parameters.id,
           ...vehicle,
@@ -38,34 +82,4 @@ const register = (experiment: Experiment, socket: Socket): any[] => {
         socket.volatile.emit('cars', [car])
       }),
   ]
-}
-
-export { register }
-function processWindow(win: Observable<Vehicle>): Observable<Vehicle> {
-  return win.pipe(
-    groupBy((car) => car.id),
-    mergeMap((cars) => cars.pipe(last()))
-  );
-}
-
-function filterCars(car: Vehicle): boolean {
-  if (!car) return false;
-  if (car.vehicleType === 'bus' && !socket.data.emitBusUpdates) return false;
-  if (car.vehicleType === 'taxi' && !socket.data.emitTaxiUpdates) return false;
-  if (car.vehicleType === 'car' && !socket.data.emitCars) return false;
-  return true;
-}
-
-function toJsonWithExperimentId(vehicle: Vehicle): any {
-  return {
-    experimentId: experiment.parameters.id,
-    ...vehicle.toJson(),
-  };
-}
-
-function emitCars(socket: Socket) {
-  return (cars: any[]) => {
-    if (!cars.length) return;
-    socket.volatile.emit('cars', cars);
-  };
 }
