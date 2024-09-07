cca8c35c17d53f2310c177573cef732dc5c4fc09
Christian Landgren <christian@landgren.nu>
2024-09-07 11:19:10 +0200

    feat: add default socket methods to SocketIOContext for error handling

--- packages/visualisation/src/context/socketIOContext.tsx
+++ packages/visualisation/src/context/socketIOContext.tsx
@@ -1,5 +1,15 @@
+import { emit } from 'process'
 import { createContext } from 'react'
 
-const SocketIOContext = createContext()
+const SocketIOContext = createContext({
+  socket: {
+    on: () => {
+      throw new Error('SocketIOContext not found')
+    },
+    emit: () => {
+      throw new Error('SocketIOContext not found')
+    },
+  },
+})
 
 export { SocketIOContext }
