88b38d0ae95a0df5d08fada3fa16a2339dfb9a27
Christian Landgren <christian@landgren.nu>
2024-09-10 09:55:15 +0200

    feat: add municipalities data to stockholm region definition

--- packages/simulator/streams/regions/stockholm.ts
+++ packages/simulator/streams/regions/stockholm.ts
@@ -1,13 +1,16 @@
 import Municipality from '../../models/Municipality'
 import Region from '../../models/Region'
 import { filter, Observable, shareReplay } from 'rxjs'
-
+import municipalitiesJson from '.../../../data/municipalities.json'
 const includedMunicipalities = ['Södertälje kommun']
 
-const stockholm = (municipalitiesStream: Observable<Municipality>) => {
+const stockholm = (municipalities: Observable<Municipality>) => {
   return new Region({
     id: 'stockholm',
     name: 'Stockholm',
+    ...municipalitiesJson.find(
+      (municipality) => municipality.namn === 'Stockholm'
+    ),
     municipalities: municipalities.pipe(
       filter((municipality) =>
         includedMunicipalities.includes(municipality.name)
